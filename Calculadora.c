2512d0c18fd43d49505bcf32340466d3a3d69012
Yukitte <80960360+Yukitte@users.noreply.github.com>
2023-08-22 10:01:42 -0300

    Calculadora.c

--- Calculadora.c
+++ Calculadora.c
@@ -1,28 +1,49 @@
+
 #include <stdio.h>
 
 int main (){
-//Coleta de dados
- float numero1;
- float numero2;
+    /*Operadores aritmeticos 
+    soma(+)
+    subtração (-)
+    divisão(/)
+    multiplicação(*)
+    modulo(%) */
+    
+//Declaraçao de variaveis
+ int num1, num2, soma, sub, mult, mod, pot;
+ float div;
 
 //Pedido de dados
 
-printf("---------------//---------------\n");
+printf("________________________________\n");
 printf("Digite os numeros para somar:\n");
 printf("Numero1:");
-scanf("%f",&numero1);
+scanf("%d",&num1);
 
 printf("Numero2:");
-scanf("%f",&numero2);
+scanf("%d",&num2);
 
 //somando
-float soma = numero1 + numero2;
+soma = num1 + num2;
+sub = num1 - num2;
+mult = num1 * num2;
+mod = num1 % num2;
+pot = pow(num1,num2);
+
+div = (float)num1/(float)num2;
 
 //Puxando o resultado
-printf("---------------//---------------\n");
+printf("--------------------------------\n");
 printf("\nResultado:");
-printf("%.0f + %.0f = %.0f",numero1 ,numero2, soma);    
+printf("\nSoma:%d",soma);
+printf("\nDivisão:%.0f",div);
+printf("\nSubtração:%d",sub);
+printf("\nModulo:%d%",mod);
+printf("\nMultiplicação:%d",mult);
+printf("\nPotência:%d",pot);
 
 
+
+printf("\n________________________________\n");
     return 0;
-}
+} 
