a604645b921c770c0299cd4e8dd873ee16856032
Yukitte <80960360+Yukitte@users.noreply.github.com>
2023-08-23 21:54:14 -0300

    ex7.c

--- ex7.c
+++ ex7.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
 int main (){
-    int dia,mes,ano,total;
+    int dia,mes,ano,dia1,mes1,ano1,total1,total,resultado ;
 
 
 printf("\n________________________________\n");
 printf("digite dia mes e ano do seu Nascimento:");
 scanf("%d %d %d", &dia, &mes, &ano);
-
+printf("Atual :");
+scanf("%d %d %d", &dia1, &mes1, &ano1);
 
 total = dia + mes*30 + ano*365;
+total1 = dia1 + mes1*30 + ano1*365;
+resultado = total1 -total;
+
 printf("\n------------------------------");
 printf("\nDias de vida: %d",total);
+printf("\nDias de vida: %d",resultado);
 printf("\n________________________________\n");
 
 
